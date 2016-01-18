4151cdac7ceaa4e925f52d4b22d81a19382a3b6f
Ankur Patel <ankur@embertec.com>
2016-01-18 17:03:37 +1030

    Remove title attribute from button

--- bootstrap-iconpicker/js/bootstrap-iconpicker.js
+++ bootstrap-iconpicker/js/bootstrap-iconpicker.js
@@ -215,7 +215,7 @@
                 var btn = $('<button class="btn ' + op.unselectedClass + ' btn-icon"></button>').hide();
                 if (pos < op.icons.length) {
                     var v = op.iconClassFix + op.icons[pos];
-                    btn.val(v).attr('title', v).append('<i class="' + op.iconClass + ' ' + v + '"></i>').show();
+                    btn.val(v).append('<i class="' + op.iconClass + ' ' + v + '"></i>').show();
                     if (op.icon === v) {
                         btn.addClass(op.selectedClass).addClass('btn-icon-selected');
                     }
